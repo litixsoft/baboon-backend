38f5483092d9869f946796dcd0fc13974e7bf788
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2015-01-14 15:28:08 +0100

    feat: save http verb in rights collection

--- scripts/setup.js
+++ scripts/setup.js
@@ -10,15 +10,19 @@ var options = {};
 options.config = config;
 
 var mongo = require('../lib/start.d/lxMongoDb');
-mongo(options, function() {
+mongo(options, function () {
 
     var auth = require('../lib/start.d/lxAuth');
     auth(options, function () {
 
         console.log('Refreshing rights in database..');
 
-        options.auth.refreshRightsInDb(function () {
+        options.auth.refreshRightsInDb(function (err, res) {
             options.db.disconnect();
+
+            if (res.processRoles) {
+                console.log('Rights created: ' + res.processRoles);
+            }
 
             console.log('Done.');
         });
